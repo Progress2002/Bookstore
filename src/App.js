1abfcb8cf229424cca784372a8b946a24847b879
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-01-19 19:23:47 +0100

    Update App.js to fuction component and fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Categories from './Pages/Categories';
 import Books from './Pages/Books';
-import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    books: [
-      {title: 'A Game of Thrones', author: 'George Raymond Richard Martin'},
-      {title: 'The Lord of the Rings', author: 'John Ronald Reuel Tolkien'},
-      {title: 'Harry Potter', author: 'Joanne Kathleen Rowling'}
-    ]
-  };
-
-  render() {
-    return (
-      <>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Books books={this.state.books} />} />
-          <Route path='/categories' element={<Categories />} />
-        </Routes>
-      </>
-    )
-  }
-};
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  </>
+);
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-  <div>
-    <div className="logo">
-      <h1>Bookstore CMS</h1>
+    <div>
+      <div className="logo">
+        <h1>Bookstore CMS</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">BOOKS</Link>
+          </li>
+          <li>
+            <Link to="/categories">CATEGORIES</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">BOOKS</Link>
-        </li>
-        <li>
-          <Link to="/categories">CATEGORIES</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-  <div></div>
-</header>
+    <div />
+  </header>
 );
 
-export default Header
+export default Header;
