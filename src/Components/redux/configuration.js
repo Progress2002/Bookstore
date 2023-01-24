6ff9348068920b8514e19d2f831da0ae402d13bf
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-01-24 13:54:34 +0100

    configure redux Store to import necessary methods from Redux Toolkit

---
+++ src/Components/redux/configuration.js
@@ -0,0 +1,17 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import booksReducer from "./books/books";
+import categoriesReducer from "./categories/categories";
+
+// combine all reducers together
+
+const reducer = combineReducers({
+  book: booksReducer,
+  categories: categoriesReducer,
+});
+
+// update state in store
+const store = configureStore({
+  reducer,
+});
+
+export default store;
---
+++ src/Components/redux/types/types.js
@@ -0,0 +1,9 @@
+const  ADDED_BOOK = 'redux/books/BOOK_ADDED';
+const  REMOVE_BOOK = 'redux/books/BOOK_REMOVED';
+const  BOOK_STATUS_CHECK = 'redux/categories/CHECKED_BOOK_STATUS';
+
+export default {
+  ADDED_BOOK,
+  REMOVE_BOOK,
+  BOOK_STATUS_CHECK
+};
