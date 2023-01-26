7c0b814ca7377d52d087ea10160b9e6b60668fab
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-01-26 11:41:44 +0100

    Add remove  function to remove btn

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
-  const { Title, Author } = props;
+  const { Title, Author, id } = props;
+  const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
     <li className="book_item">
       <h2>{Title}</h2>
       <h4>{Author}</h4>
-      <button type="button">Remove</button>
+      <button type="button" onClick={handleRemove}>Remove</button>
     </li>
   );
 };
@@ -15,6 +23,7 @@ const Book = (props) => {
 Book.propTypes = {
   Title: PropTypes.string.isRequired,
   Author: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default Book;
