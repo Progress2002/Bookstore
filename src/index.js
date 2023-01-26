453705728a017420f83f8c5c7bb974b294c2912f
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-01-26 11:36:50 +0100

    Import provider  and connect app to redux

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-      <App />
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
